de8371ccc1e488a674a6fd9cf629fce7a894fa6b
Omar Al-Hamawi <110392397+OmarAlHamawi@users.noreply.github.com>
2025-06-18 18:06:30 +0300

    Update Login.tsx

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "../Css/login.css";
+import "../Css/Login.css";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 import logo from "../../images/logo/logo2.png";
 import { Link, useNavigate } from "react-router-dom";
